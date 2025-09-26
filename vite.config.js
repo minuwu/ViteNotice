5066c1dcbf630129a2e8292497fc7093c8d2fa35
Mohammad Minhazul Abedin <84508269+minuwu@users.noreply.github.com>
2025-09-26 12:17:13 +0600

    updated base link for pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   // base: './',
-  base: 'https://github.com/minuwu/ViteNotice/',
+  base: 'https://minuwu.github.io/ViteNotice/',
   build: {
     outDir: 'docs'
   },
